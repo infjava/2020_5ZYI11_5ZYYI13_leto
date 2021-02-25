ddcd30edb625d26ce3ee5a3e6fcd03be565aa909
Jan Janech <jan.janech@kst.uniza.sk>
2021-02-25 09:04:45 +0100

    01a - trieda Student podla zadania

---
+++ skupinaStudentov/src/Student.java
@@ -0,0 +1,28 @@
+/**
+ * 25. 2. 2021 - 8:18
+ *
+ * @author janik
+ */
+public class Student {
+    private final String osobneCislo;
+    private final String meno;
+    private final String priezvisko;
+
+    public Student(String osobneCislo, String meno, String priezvisko) {
+        this.osobneCislo = osobneCislo;
+        this.meno = meno;
+        this.priezvisko = priezvisko;
+    }
+
+    public String getOsobneCislo() {
+        return this.osobneCislo;
+    }
+
+    public String getMeno() {
+        return this.meno;
+    }
+
+    public String getPriezvisko() {
+        return this.priezvisko;
+    }
+}
