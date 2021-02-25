3f5ca856b71aecf9d0430ad17673a22281433453
Jan Janech <jan.janech@kst.uniza.sk>
2021-02-25 08:37:10 +0100

    01a - prazdny projekt pre skupinu studentov

---
+++ skupinaStudentov/src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: janik
+ * Date: 25. 2. 2021
+ * Time: 8:18
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        //TODO Write your code
+        System.out.println("Hello world");
+    }
+}
