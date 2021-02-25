355f3c5a149842ace3e3851c3595ac123eb4b66b
Jan Janech <jan.janech@kst.uniza.sk>
2021-02-25 09:22:12 +0100

    01a - pridavanie studenta do skupiny

---
+++ skupinaStudentov/src/Skupina.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+/**
+ * 25. 2. 2021 - 8:18
+ *
+ * @author janik
+ */
+public class Skupina {
+    private final String cisloSkupiny;
+    private ArrayList<Student> studenti;
+
+    public Skupina(String cisloSkupiny) {
+        this.cisloSkupiny = cisloSkupiny;
+        this.studenti = new ArrayList<>();
+    }
+
+    public void pridajStudenta(Student student) {
+        this.studenti.add(student);
+    }
+}
