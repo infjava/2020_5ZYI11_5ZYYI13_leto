2ae3ab6a714c3683ca661a45e9af39a2cf21751a
Jan Janech <jan.janech@kst.uniza.sk>
2021-03-04 09:18:31 +0100

    02a - atributy pre vychody v triede Miestnost nahradene za ArrayList

---
+++ wof/src/Vychod.java
@@ -0,0 +1,18 @@
+public class Vychod {
+    private final String smer;
+    private final Miestnost ciel;
+
+    public Vychod(String smer, Miestnost ciel) {
+
+        this.smer = smer;
+        this.ciel = ciel;
+    }
+
+    public String getSmer() {
+        return this.smer;
+    }
+
+    public Miestnost getCiel() {
+        return this.ciel;
+    }
+}
