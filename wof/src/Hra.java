bc87ea39a2c830932d072bf6ea682f4d2dc571c0
Jan Janech <jan.janech@kst.uniza.sk>
2021-03-05 09:27:48 +0100

    02b - presun vytvorenia miestnosti do novej triedy Prostredie

--- wof/src/Hra.java
+++ wof/src/Hra.java
@@ -21,40 +21,14 @@
 public class Hra  {
     private final Parser parser;
     private Miestnost aktualnaMiestnost;
-    
+
     /**
      * Vytvori a inicializuje hru.
      */
     public Hra() {
-        this.vytvorMiestnosti();
+        Prostredie prostredie = new Prostredie();
+        this.aktualnaMiestnost = prostredie.getStartovaciaMiestnost();
         this.parser = new Parser();
-    }
-
-    /**
-     * Vytvori mapu hry - miestnosti.
-     */
-    private void vytvorMiestnosti() {
-        // vytvorenie miestnosti
-        Miestnost terasa = new Miestnost("terasa - hlavny vstup na fakultu");
-        Miestnost aula = new Miestnost("aula");
-        Miestnost bufet = new Miestnost("bufet");
-        Miestnost labak = new Miestnost("pocitacove laboratorium");
-        Miestnost kancelaria = new Miestnost("kancelaria spravcu pocitacoveho laboratoria");
-        
-        // inicializacia miestnosti = nastavenie vychodov
-        terasa.nastavVychod("vychod", aula);
-        terasa.nastavVychod("juh", labak);
-        terasa.nastavVychod("zapad", bufet);
-        aula.nastavVychod("zapad", terasa);
-        bufet.nastavVychod("vychod", terasa);
-        labak.nastavVychod("sever", terasa);
-        labak.nastavVychod("vychod", kancelaria);
-        kancelaria.nastavVychod("zapad", labak);
-
-        aula.nastavVychod("dole", bufet);
-        bufet.nastavVychod("hore", aula);
-
-        this.aktualnaMiestnost = terasa;  // startovacia miestnost hry
     }
 
     /**
