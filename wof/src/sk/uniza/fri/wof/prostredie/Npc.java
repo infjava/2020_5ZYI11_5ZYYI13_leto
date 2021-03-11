b81c182a3a391748a8e2bddbada4c308088d29f7
Jan Janech <jan.janech@kst.uniza.sk>
2021-03-11 09:44:25 +0100

    03a - rozhovor s vratnickou

--- wof/src/sk/uniza/fri/wof/prostredie/Npc.java
+++ wof/src/sk/uniza/fri/wof/prostredie/Npc.java
@@ -1,5 +1,7 @@
 package sk.uniza.fri.wof.prostredie;
 
+import java.util.Scanner;
+
 public class Npc {
     private final String meno;
 
@@ -12,6 +14,43 @@ public class Npc {
     }
 
     public void hovor() {
-        System.out.printf("Hovoris s NPC %s%n", this.meno);
+        Scanner vstup = new Scanner(System.in);
+        System.out.println("Ahoj, ja som tu nejaka vratnicka. A ty si?");
+        System.out.print("> ");
+        vstup.nextLine();
+        System.out.println("Tak cau.");
+        System.out.println("1. Ako sa mas?");
+        System.out.println("2. Nemas pre mna nejaku ulohu?");
+        System.out.println("0. Ukoncit rozhovor");
+        System.out.print("Vyber moznost > ");
+        switch (vstup.nextLine()) {
+            case "1":
+                System.out.println("Vsetko ma boli, tak nezavadzaj");
+                System.out.println("0. Ukonci rozhovor");
+                System.out.print("Vyber moznost > ");
+                vstup.nextLine();
+                break;
+            case "2":
+                System.out.println("Tak ja neviem, dones mi aspirin.");
+                System.out.println("1. Lekarne zavreli kvoli covid");
+                System.out.println("2. Skusim nejaky najst");
+                System.out.println("0. Ukonci rozhovor");
+                System.out.print("Vyber moznost > ");
+                switch (vstup.nextLine()) {
+                    case "1":
+                        System.out.println("Joj, tak padaj prec.");
+                        System.out.println("0. Ukonci rozhovor");
+                        System.out.print("Vyber moznost > ");
+                        vstup.nextLine();
+                        break;
+                    case "2":
+                        System.out.println("Ach to budes zlaty.");
+                        System.out.println("0. Ukonci rozhovor");
+                        System.out.print("Vyber moznost > ");
+                        vstup.nextLine();
+                        break;
+                }
+                break;
+        }
     }
 }
