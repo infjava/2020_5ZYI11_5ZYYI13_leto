63425ea9d6e4b4e933da07d32976de850b3bc5a6
Jan Janech <jan.janech@kst.uniza.sk>
2021-03-11 08:03:39 +0100

    02HW - miestnosti podla navrhu mapy hry

--- wof/src/sk/uniza/fri/wof/prostredie/Prostredie.java
+++ wof/src/sk/uniza/fri/wof/prostredie/Prostredie.java
@@ -5,26 +5,51 @@ public class Prostredie {
 
     public Prostredie() {
         // vytvorenie miestnosti
-        Miestnost terasa = new Miestnost("terasa - hlavny vstup na fakultu");
-        Miestnost aula = new Miestnost("aula");
-        Miestnost bufet = new Miestnost("bufet");
-        Miestnost labak = new Miestnost("pocitacove laboratorium");
-        Miestnost kancelaria = new Miestnost("kancelaria spravcu pocitacoveho laboratoria");
+        Miestnost ra6 = new Miestnost("RA006 - krasna ucebna, plna pocitacov, mysi a potkanov");
+        Miestnost chodbaA = new Miestnost("chodba a - dlha chodba so svetlom monitorov na konci");
+        Miestnost wcA = new Miestnost("wc - nezabudni si zobrat mobil, nech sa nenudis");
+        Miestnost jedalen = new Miestnost("jedalen - rozvoniava tu jedlo z krabicky");
+        Miestnost chodbaC = new Miestnost("chodba c - podzemna chodba");
+        Miestnost vestibula = new Miestnost("vestibula - tu zacinas svoju put");
+        Miestnost ic = new Miestnost("ic - vdaka mnozstvu knih naokolo tu kazdy vyzera kusok inteligentnejsie");
+        Miestnost wcC = new Miestnost("wc - ked musis, tak musis - aj v podzemi");
+        Miestnost aula = new Miestnost("aula - nieco ako muciaren");
+        Miestnost chodbaB = new Miestnost("chodba b - temna chodba");
+        Miestnost chillZone = new Miestnost("chill zone - ak niekto nema internet zo sebou, tu nejaky najde vzdy");
 
         // inicializacia miestnosti = nastavenie vychodov
-        terasa.nastavVychod("vychod", aula);
-        terasa.nastavVychod("juh", labak);
-        terasa.nastavVychod("zapad", bufet);
-        aula.nastavVychod("zapad", terasa);
-        bufet.nastavVychod("vychod", terasa);
-        labak.nastavVychod("sever", terasa);
-        labak.nastavVychod("vychod", kancelaria);
-        kancelaria.nastavVychod("zapad", labak);
+        ra6.nastavVychod("vychod", chodbaA);
 
-        aula.nastavVychod("dole", bufet);
-        bufet.nastavVychod("hore", aula);
+        chodbaA.nastavVychod("zapad", ra6);
+        chodbaA.nastavVychod("vychod", wcA);
+        chodbaA.nastavVychod("juh", vestibula);
+        chodbaA.nastavVychod("dole", chodbaC);
 
-        this.startovaciaMiestnost = terasa;
+        wcA.nastavVychod("zapad", chodbaA);
+
+        jedalen.nastavVychod("vychod", chodbaC);
+
+        chodbaC.nastavVychod("zapad", jedalen);
+        chodbaC.nastavVychod("zapad2", wcC);
+        chodbaC.nastavVychod("juh", aula);
+        chodbaC.nastavVychod("hore", chodbaA);
+
+        vestibula.nastavVychod("sever", chodbaA);
+        vestibula.nastavVychod("vychod", ic);
+        vestibula.nastavVychod("juh", chodbaB);
+
+        ic.nastavVychod("zapad", vestibula);
+
+        wcC.nastavVychod("vychod", chodbaC);
+
+        aula.nastavVychod("sever", chodbaC);
+
+        chodbaB.nastavVychod("sever", vestibula);
+        chodbaB.nastavVychod("juh", chillZone);
+
+        chillZone.nastavVychod("sever", chodbaB);
+
+        this.startovaciaMiestnost = vestibula;
     }
 
     public Miestnost getStartovaciaMiestnost() {
